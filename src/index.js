2a4a5dabec7a607309aceab3801ee59b6725981b
kulvund <kulvund@hotmail.com>
2020-09-10 08:37:38 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+//src/index.js
+
+import * as styles from './shared/styles';
+import * as animation from './shared/animation';
+
+export { styles, animation };
+
+export * from './components/Button/Button';
