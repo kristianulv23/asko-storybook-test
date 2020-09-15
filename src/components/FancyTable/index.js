385785eac3976422738d0c6948ea1cfc8e485348
kulvund <kulvund@hotmail.com>
2020-09-15 10:46:10 +0200

    Update table package

---
+++ src/components/FancyTable/index.js
@@ -0,0 +1,5 @@
+export { default as FancyTable } from "./FancyTable";
+export { default as FancyTableBody } from "./FancyTableBody";
+export { default as FancyTableCell } from "./FancyTableCell";
+export { default as FancyTableHead } from "./FancyTableHead";
+export { default as FancyTableRow } from "./FancyTableRow";
