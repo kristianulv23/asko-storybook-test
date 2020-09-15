1691026660ea961bdaa2f8b4373b465824a083ea
kulvund <kulvund@hotmail.com>
2020-09-15 10:34:47 +0200

    Update path index

--- packages/FancyTable/lib/index.js
+++ packages/FancyTable/lib/index.js
@@ -1,5 +1,5 @@
-export { default as FancyTable } from "@/src/components/FancyTable/FancyTable";
-export { default as FancyTableBody } from "@/src/components/FancyTable/FancyTableBody";
-export { default as FancyTableCell } from "@/src/components/FancyTable/FancyTableCell";
-export { default as FancyTableHead } from "@/src/components/FancyTable/FancyTableHead";
-export { default as FancyTableRow } from "@/src/components/FancyTable/FancyTableRow";
+export { default as FancyTable } from "../../../src/components/FancyTable/FancyTable";
+export { default as FancyTableBody } from "../../../src/components/FancyTable/FancyTableBody";
+export { default as FancyTableCell } from "../../../src/components/FancyTable/FancyTableCell";
+export { default as FancyTableHead } from "../../../src/components/FancyTable/FancyTableHead";
+export { default as FancyTableRow } from "../../../src/components/FancyTable/FancyTableRow";
